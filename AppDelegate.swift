9b294aae275b22812e7100eeb89ceb762ee2a87a
Raymond Diamonds <raymond.diamonds@yahoo.ca>
2017-06-26 15:41:53 -0700

    Change login view controller background to orange

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -17,6 +17,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
+        
+        //1
+        let storyboard = UIStoryboard(name: "Login", bundle: .main)
+        
+        //2
+        if let initialViewController = storyboard.instantiateInitialViewController(){
+            //3
+            window?.rootViewController = initialViewController
+            //4
+            window?.makeKeyAndVisible()
+        }
         return true
     }
 
---
+++ LoginViewController.swift
@@ -0,0 +1,9 @@
+//
+//  LoginViewController.swift
+//  Makestagram
+//
+//  Created by Raymond Diamonds on 2017-06-26.
+//  Copyright © 2017 Raymond Diamonds. All rights reserved.
+//
+
+import Foundation
