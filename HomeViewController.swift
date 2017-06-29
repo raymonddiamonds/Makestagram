9342de03fccb3bcde46569b4489854d38146c79f
Raymond Diamonds <raymond.diamonds@yahoo.ca>
2017-06-29 09:55:55 -0700

    Add tab bar items and connect view controllers

---
+++ HomeViewController.swift
@@ -0,0 +1,9 @@
+//
+//  HomeViewController.swift
+//  Makestagram
+//
+//  Created by Raymond Diamonds on 2017-06-29.
+//  Copyright © 2017 Raymond Diamonds. All rights reserved.
+//
+
+import Foundation
