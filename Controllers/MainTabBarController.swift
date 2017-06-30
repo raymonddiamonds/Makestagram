282f80e1284fc6abfe64c2b36f5bc679f7bbcd0f
Raymond Diamonds <raymond.diamonds@yahoo.ca>
2017-06-30 11:00:30 -0700

    Implement photo taking function

---
+++ Controllers/MainTabBarController.swift
@@ -0,0 +1,41 @@
+//
+//  MainTabBarController.swift
+//  Makestagram
+//
+//  Created by Raymond Diamonds on 2017-06-29.
+//  Copyright © 2017 Raymond Diamonds. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class MainTabBarController: UITabBarController {
+    
+    // MARK: - Properties
+    
+    let photoHelper = MGPhotoHelper()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        photoHelper.completionHandler = { image in
+            print("handle image")
+        }
+        
+        delegate = self
+        tabBar.unselectedItemTintColor = .black
+    }
+
+}
+
+extension MainTabBarController: UITabBarControllerDelegate {
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        if viewController.tabBarItem.tag == 1 {
+            photoHelper.presentActionSheet(from: self)
+            return false
+        }
+        
+        return true
+    }
+}
---
+++ MGPhotoHelper.swift
@@ -0,0 +1,9 @@
+//
+//  MGPhotoHelper.swift
+//  Makestagram
+//
+//  Created by Raymond Diamonds on 2017-06-29.
+//  Copyright © 2017 Raymond Diamonds. All rights reserved.
+//
+
+import Foundation
